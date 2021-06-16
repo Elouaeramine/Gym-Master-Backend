82d752499f897e6e8ba889c34c1c34a0395dfa38
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-06-16 11:56:56 +0100

    fixed Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
